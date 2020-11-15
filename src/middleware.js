79ae232f84361d10881e0ce31da88caa9e8aef75
ersoma <info@somaerdelyi.net>
2020-11-15 12:21:17 +0100

    Refactor middleware to use class syntax

--- src/middleware.js
+++ src/middleware.js
@@ -1,35 +1,52 @@
-/* eslint no-unused-vars: 0 */
 'use strict';
 
 const Joi = require('joi');
 
-module.exports = (schema, {
-  onError = 'validation failed for given schema'
-} = {}) => {
+class KoaJoiValidatorMiddleware {
+  constructor(schema, {
+    onError = 'validation failed for given schema'
+  }) {
+    this.schema = schema;
+    this.onError = onError;
 
-  if (!schema) {
-    throw new Error('schema parameter is missing');
+    this._validateParameters();
   }
 
-  const onErrorIsValid = ['string', 'function'].filter(v => v === typeof onError).length === 1;
-  if (onErrorIsValid === false) {
-    throw new Error('onError parameter is invalid');
-  }
-
-  return async (ctx, next) => {
+  async execute(ctx, next) {
+    this.ctx = ctx;
+    this.next = next;
     try {
-      Joi.attempt(ctx.request.body, schema);
+      Joi.attempt(ctx.request.body, this.schema);
       await next();
     } catch (error) {
-      if (error instanceof Joi.ValidationError === false) {
-        throw error;
-      }
-      if (typeof onError === 'string') {
-        throw new Error(onError);
-      } else if (typeof onError === 'function') {
-        onError(ctx, next, error);
-      }
+      this._handleValidationError(error);
     }
-  };
+  }
 
+  _validateParameters() {
+    if (!this.schema) {
+      throw new Error('schema parameter is missing');
+    }
+
+    const onErrorIsValid = ['string', 'function'].filter(v => v === typeof this.onError).length === 1;
+    if (onErrorIsValid === false) {
+      throw new Error('onError parameter is invalid');
+    }
+  }
+
+  _handleValidationError(error) {
+    if (error instanceof Joi.ValidationError === false) {
+      throw error;
+    }
+    if (typeof this.onError === 'string') {
+      throw new Error(this.onError);
+    } else if (typeof this.onError === 'function') {
+      this.onError(this.ctx, this.next, error);
+    }
+  }
+}
+
+module.exports = (schema, config = {}) => {
+  const instance = new KoaJoiValidatorMiddleware(schema, config);
+  return async (ctx, next) => instance.execute(ctx, next);
 };
