9ae76abb77b88c9b09bfd93cf53b78ee4a27822c
ersoma <info@somaerdelyi.net>
2020-11-15 18:24:04 +0100

    Refactor tests, extract common code

--- src/middleware.spec.js
+++ src/middleware.spec.js
@@ -6,6 +6,10 @@ const middlewareFactory = require('./middleware');
 const { expect } = require('chai');
 
 describe('koaJoiValidatorMiddleware', () => {
+  const schema = Joi.object({ param1: Joi.string().required() });
+  const fakeInvalidCtx = { request: { body: { param1: 1234 } } };
+  const fakeValidCtx = { request: { body: { param1: 'test' } } };
+  const fakeNext = () => {};
 
   describe('works as a Koa middleware', () => {
     it('should export a factory function', () => {
@@ -25,56 +29,41 @@ describe('koaJoiValidatorMiddleware', () => {
 
   describe('uses default parameters', () => {
     it('should call next if body is valid with given schema', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
-      const fakeCtx = { request: { body: { param1: 'test' } } };
       const next = global.sandbox.stub();
 
       const middleware = middlewareFactory(schema);
-      await middleware(fakeCtx, next);
+      await middleware(fakeValidCtx, next);
 
       expect(next).to.be.calledOnce;
     });
 
     it('should throw error if body is invalid with given schema', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
-      const fakeCtx = { request: { body: { param1: 1234 } } };
-      const fakeNext = () => {};
-
       const middleware = middlewareFactory(schema);
-      await expect(middleware(fakeCtx, fakeNext)).to.be.rejectedWith('validation failed for given schema');
+      await expect(middleware(fakeInvalidCtx, fakeNext)).to.be.rejectedWith('validation failed for given schema');
     });
   });
 
   describe('uses custom onError parameters', async () => {
     it('should throw custom error if body is invalid with given schema', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
-      const fakeCtx = { request: { body: { param1: 1234 } } };
-      const fakeNext = () => {};
       const customError = 'this is not good';
 
       const middleware = middlewareFactory(schema, { onError: customError });
-      await expect(middleware(fakeCtx, fakeNext)).to.be.rejectedWith(customError);
+      await expect(middleware(fakeInvalidCtx, fakeNext)).to.be.rejectedWith(customError);
     });
 
     it('should call onError function if body is invalid with given schema', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
-      const fakeCtx = { request: { body: { param1: 1234 } } };
-      const fakeNext = () => {};
       const customErrorSpy = global.sandbox.spy();
-
-      const validationError = schema.validate(fakeCtx.request.body);
+      const validationError = schema.validate(fakeInvalidCtx.request.body);
 
       const middleware = middlewareFactory(schema, { onError: customErrorSpy });
-      await middleware(fakeCtx, fakeNext);
-      expect(customErrorSpy).to.be.calledOnce;
-      expect(customErrorSpy.args[0][0]).to.be.eql(fakeCtx);
+      await middleware(fakeInvalidCtx, fakeNext);
+      expect(customErrorSpy.args[0][0]).to.be.eql(fakeInvalidCtx);
       expect(customErrorSpy.args[0][1]).to.be.eql(fakeNext);
       expect(customErrorSpy.args[0][2].toString()).to.be.eql(validationError.error.toString());
       expect(customErrorSpy.args[0][2] instanceof Joi.ValidationError).to.be.true;
     });
 
     it('should throw error if factory\'s onError parameter is not a supported type', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
       const customError = { objectType: 'is not supported' };
 
       const shouldThrow = () => {
@@ -86,7 +75,6 @@ describe('koaJoiValidatorMiddleware', () => {
 
   describe('uses custom getSubject parameters', () => {
     it('should throw error if getSubject is not a function', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
       const customGetSubject = 'String';
 
       const shouldThrow = () => {
@@ -96,10 +84,8 @@ describe('koaJoiValidatorMiddleware', () => {
     });
 
     it('should validate the object returned by getSubject', async () => {
-      const schema = Joi.object({ param1: Joi.string().required() });
       const customGetSubject = ctx => ctx.state;
       const fakeCtx = { state: { param1: 'string' } };
-      const fakeNext = () => {};
 
       const middleware = middlewareFactory(schema, { getSubject: customGetSubject });
       await expect(middleware(fakeCtx, fakeNext)).to.be.fulfilled;
