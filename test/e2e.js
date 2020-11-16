ad6b69deb1eb20024d5976a4cf265d4038bdce10
ersoma <info@somaerdelyi.net>
2020-11-16 08:22:24 +0100

    Add more e2e tests, extract common code

--- test/e2e.js
+++ test/e2e.js
@@ -9,33 +9,63 @@ const { expect } = require('chai');
 const PORT = 8080;
 const URL = `http://localhost:${PORT}`;
 
-describe('tests middleware with default parameters', () => {
+describe('Tests koaJoiValidatorFactory middleware', () => {
 
   let server;
+  let middleware;
   const schema = Joi.object({ param1: Joi.string().required() }).required();
-  const middleware = koaJoiValidatorFactory(schema);
-
-  afterEach(() => {
-    server.stopFakeServer();
-  });
 
   beforeEach(async () => {
     server = new FakeServer([middleware]);
     await server.startFakeServer(PORT);
   });
+  afterEach(() => server.stopFakeServer());
 
-  it('shoud respond 500 if body is empty', async () => {
-    let thrownError;
-    try {
-      await axios.post(URL);
-    } catch (error) {
-      thrownError = error;
-    }
-    expect(thrownError.response.status).to.be.eql(500);
+  describe('with default parameters', () => {
+    before(() => middleware = koaJoiValidatorFactory(schema));
+
+    it('shoud respond 500 if body is empty', async () => {
+      let thrownError;
+      try {
+        await axios.post(URL);
+      } catch (error) {
+        thrownError = error;
+      }
+      expect(thrownError.response.status).to.be.eql(500);
+    });
+
+    it('shoud respond 200 if body is correct', async () => {
+      const response = await axios.post(URL, { param1: 'hello' });
+      expect(response.status).to.be.eql(200);
+    });
   });
 
-  it('shoud respond 200 if body is correct', async () => {
-    const response = await axios.post(URL, { param1: 'hello' });
-    expect(response.status).to.be.eql(200);
+  describe('with custom getSubject parameters', () => {
+    before(() => middleware = koaJoiValidatorFactory(schema, { getSubject: ctx => ctx.request.query }));
+
+    it('should respond 500 if query string is not correct', async () => {
+      let thrownError;
+      try {
+        await axios.get(URL);
+      } catch (error) {
+        thrownError = error;
+      }
+      expect(thrownError.response.status).to.be.eql(500);
+    });
+
+    it('shoud respond 200 if query string is correct', async () => {
+      const response = await axios.get(`${URL}?param1=test`);
+      expect(response.status).to.be.eql(200);
+    });
+  });
+
+  describe('with custom onError parameters', () => {
+    const errorMessage = 'Invalid body, please try again';
+    before(() => middleware = koaJoiValidatorFactory(schema, { onError: ctx => ctx.body = errorMessage }));
+
+    it('should respond with custom response if body is missing', async () => {
+      const response = await axios.post(URL, {});
+      expect(response.data).to.be.eql(errorMessage);
+    });
   });
 });
